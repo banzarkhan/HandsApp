dc705bb892d7e5869bfef4fd8320c7549cfe0162
banzarkhan <banzarhanova93@gmail.com>
2023-02-25 13:32:33 +0100

    Add existing project files to Git

---
+++ HadsApp/HadsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HadsAppApp.swift
+//  HadsApp
+//
+//  Created by Ariuna Banzarkhanova on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HadsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
